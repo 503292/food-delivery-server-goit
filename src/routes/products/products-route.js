ef8bf27dd23ec58956df22e4ffa8c36f9cc60aa1
Kalysh <503292@ukr.net>
2019-11-15 13:09:58 +0200

    ADD _ view to response all-products

--- src/routes/products/products-route.js
+++ src/routes/products/products-route.js
@@ -1,13 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-// асинхронное чтение
 const productsRoute = (request, response) => {
   let reqPath = path.join(__dirname, "../../db/products/all-products.json");
-  let fileReaded = fs.readFile(`${reqPath}`, "utf8", function(error, data) {
-    console.log("Асинхронное чтение файла");
-    if (error) throw error;
-    console.log(data);
+
+  fs.readFile(`${reqPath}`, "utf8", function(error, data) {
+    if (!error) {
+      console.log("Success" + data);
+
+      const jsonObj = JSON.parse(data); // Parse Data to JSON
+
+      response.end(data); //Send back as Response
+    } else {
+      response.end("Error: " + error); //Handle Error
+    }
   });
 };
 
