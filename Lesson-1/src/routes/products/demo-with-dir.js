0a3a619a76287ca3177fbfae8a37698c030608bc
Kalysh <503292@ukr.net>
2019-11-21 15:39:07 +0200

    ADD _ logic to read DIR

--- Lesson-1/src/routes/products/demo-with-dir.js
+++ Lesson-1/src/routes/products/demo-with-dir.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
 
+const newDirectoryPath = "../../routes";
 const newDirectoryPath2 = "../../dbTEMP/1/2/3";
 
 // create DIR
 // recursive => create folders one after another
+
 // fs.mkdir(newDirectoryPath2, { recursive: true }, err => {
 //   console.log("create DIR");
 // });
+
+// read DIR
+const file = fs.readdirSync(__dirname);
+console.log(file, "DIR is read");
