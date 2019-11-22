62e29b7a8b7585365a839ac394e5854662827b54
Kalysh <503292@ukr.net>
2019-11-22 17:31:36 +0200

    ADD _ logic for GET productsById

--- Lesson-2/src/routes/products/send-product.js
+++ Lesson-2/src/routes/products/send-product.js
@@ -1,20 +1,68 @@
-const url = require('url');
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
 const getId = url => {
-  const lastIndex = url.lastIndexOf('/');
+  const lastIndex = url.lastIndexOf("/");
 
   if (lastIndex !== -1) {
-    return url.slice(lastIndex +1);
+    return url.slice(lastIndex + 1);
   }
 };
 
 const getProducts = (request, response) => {
   const parsedUrl = url.parse(request.url);
   const id = getId(parsedUrl.path);
+  let filePath = path.join(
+    __dirname,
+    "../../..",
+    "data",
+    "products",
+    "all-products.json"
+  );
 
-  response.writeHead(200, {"Content-Type": "application/json"});
-  response.write(JSON.stringify({ productid: id }));
+  const allProducts = fs.readFileSync(filePath, "utf8");
+
+  const parseProducts = JSON.parse(allProducts);
+
+  const resultById = parseProducts.find(prod => prod.id == id);
+  if (resultById === undefined) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify({ status: "no products", products: [] }));
+    response.end();
+    return;
+  }
+
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.write(JSON.stringify({ status: "success", products: resultById }));
   response.end();
 };
 
 module.exports = getProducts;
+
+// const url = require("url");
+
+// const getId = url => {
+//   const lastIndex = url.lastIndexOf("/");
+//   if (lastIndex !== -1) {
+//     return url.slice(lastIndex + 1);
+//   }
+// };
+
+// const getProducts = (request, response) => {
+//   const parsedUrl = url.parse(request.url);
+//   console.log(parsedUrl);
+//   const id = getId(parsedUrl.path);
+//   console.log(id, "id");
+
+//   // if (id === 0) {
+//   //   console.log("products --->");
+//   //   return;
+//   // }
+
+//   response.writeHead(200, { "Content-Type": "application/json" });
+//   response.write(JSON.stringify({ productid: id }));
+//   response.end();
+// };
+
+// module.exports = getProducts;
--- Lesson-2/src/routes/router.js
+++ Lesson-2/src/routes/router.js
@@ -2,14 +2,12 @@ const mainRoute = require("./main/main");
 const imageRoute = require("./image/get-image");
 const getUser = require("./user/get-user");
 const handleProductsRoute = require("./products");
-const getProductsById = require("./products/get-products-by-id");
 
 const router = {
   "/me": mainRoute,
   "/image": imageRoute,
   "/users": getUser,
   "/products": handleProductsRoute,
-  "./products/:id": getProductsById,
   "/products/add-order": handleProductsRoute,
   default: mainRoute
 };
